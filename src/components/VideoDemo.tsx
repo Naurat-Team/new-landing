64424c3997b606219438adb2e0e3c252fa051817
Julian Pira <jpira@unal.edu.co>
2025-04-02 23:06:18 -0500

    add automatic reproduction 2

--- src/components/VideoDemo.tsx
+++ src/components/VideoDemo.tsx
@@ -1,7 +1,7 @@
 const VideoDemo = () => {
     return (
         <div className="flex justify-center items-center">
-            <video className="w-full max-w-4xl rounded-2xl shadow-lg" controls autoPlay
+            <video className="w-full max-w-4xl rounded-2xl shadow-lg" autoPlay
                 loop >
                 <source src="/demo/Demo.mp4"
 
